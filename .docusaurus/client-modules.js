a572164b6f334ea31aa3f140fe08dac9177672c7
“xzkxzk123” <675332938@qq.com>
2023-01-16 14:15:34 +0800

    main

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('F:\\docusaurus\\1.9\\11.1\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('F:\\docusaurus\\1.9\\11.1\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('F:\\docusaurus\\1.9\\11.1\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('F:\\docusaurus\\1.9\\11.1\\src\\css\\custom.css'),
-];
--- .github/ISSUE_TEMPLATE/enhancement.md
+++
@@ -1,7 +0,0 @@
----
-name: "\U0001F680 Enhancement"
-about: Propose an enhancement.
-labels: enhancement
----
-
-## Enhancement
