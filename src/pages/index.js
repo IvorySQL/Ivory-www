3841294b3c4d359ee8625f8b1a4c0ce735e9538d
“xzkxzk123” <675332938@qq.com>
2023-06-01 17:54:27 +0800

    main

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,32 +7,62 @@ import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
 import Translate, {translate} from '@docusaurus/Translate';
 import Elephant from '../../svg/img-elephant-balloon.svg';
+import Slider from "react-slick";
+import Recruit from "../../static/img/index-recruit.jpg"
+import SliderIndex from './slider'
+import RecruitPhone from "../../static/img/index-recruit-phone.jpg"
+import SliderPhoneIndex from './slider-phone'
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
+     const settings = {
+      // autoplay: true,
+      // autoplaySpeed: 5000,
+      dots: true,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      };
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <div class="row">
-
-          <div className={styles.heroCenterImage}>
-          <h1 className="hero__title"><Translate>IvorySQL</Translate></h1>
-        <p className="hero__subtitle"><Translate>Open Source Oracle compatible PostgreSQL</Translate></p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="https://github.com/IvorySQL/IvorySQL/blob/master/README.md">
-            <Translate>Learn More</Translate>
-          </Link>
-        </div>
-
+     <div id='main'>
+        <Slider {...settings}>
+          <div className={clsx(styles.recruitBanner)}>
+            <div class="indexImg">
+              <img src={Recruit} className={styles.indexImg}></img>
+              <SliderIndex></SliderIndex>
+            </div>
+            <div class="indexImg-phone">
+              <img src={RecruitPhone} className={styles.indexImgPhone}></img>
+              <SliderPhoneIndex></SliderPhoneIndex>
+            </div>
           </div>
-          <div className={styles.heroRightImage}>
-            <Elephant></Elephant>
+          <div>
+            <header className={clsx('hero hero--primary', styles.heroBanner)}>
+              <div className="container">
+                <div class="row">
+        
+                  <div className={styles.heroCenterImage}>
+                  <h1 className="hero__title"><Translate>IvorySQL</Translate></h1>
+                <p className="hero__subtitle"><Translate>Open Source Oracle compatible PostgreSQL</Translate></p>
+                <div className={styles.buttons}>
+                  <Link
+                    className="button button--secondary button--lg"
+                    to="https://github.com/IvorySQL/IvorySQL/blob/master/README.md">
+                    <Translate>Learn More</Translate>
+                  </Link>
+                </div>
+        
+                  </div>
+                  <div className={styles.heroRightImage}>
+                    <Elephant></Elephant>
+                  </div>
+                </div>
+              </div>
+            </header>
           </div>
-        </div>
+        </Slider>
       </div>
-    </header>
   );
 }
 
