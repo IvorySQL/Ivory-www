fc092b547580dbe2a0f16ec3129354c068dde8ca
GreeSpirit <1329935210@qq.com>
2024-05-16 15:53:52 +0800

    feat：提交代码20240516

--- src/pages/slider-bug-phone.js
+++ src/pages/slider-bug-phone.js
@@ -10,25 +10,25 @@ export default function SliderBugphone(){
     return(
         <div className="index-bug-p">
             <div className="index-bug-content-p">
-                <div className="bug-title-p"><Translate>Woodpecker Plan</Translate></div>
+                {/* <div className="bug-title-p"><Translate>Woodpecker Plan</Translate></div> */}
                 <div className="bug-content-p">
-                    <div className="bug-svg-p"><ArrowRight></ArrowRight></div>
-                    <div className="span-p"><Translate>IvorySQL version 3.0 has been released. We invite community members to join our testing activities and assist in identifying any product issues！</Translate></div>
+                    {/* <div className="bug-svg-p"><ArrowRight></ArrowRight></div> */}
+                    {/* <div className="span-p"><Translate>IvorySQL version 3.0 has been released. We invite community members to join our testing activities and assist in identifying any product issues！</Translate></div> */}
                 </div>
                 <div className="bug-content-bottom-p">
-                    <div className="bug-svg-bottom-p"><ArrowRight></ArrowRight></div>
-                    <div className="span-p"><Translate>Participants will receive exclusive IvorySQL-branded peripherals as a token of appreciation.</Translate></div>
+                    {/* <div className="bug-svg-bottom-p"><ArrowRight></ArrowRight></div> */}
+                    {/* <div className="span-p"><Translate>Participants will receive exclusive IvorySQL-branded peripherals as a token of appreciation.</Translate></div> */}
                 </div>
                 <div className="bug-button-p">
                     <div className="button-p">
-                      <Link
+                      {/* <Link
                         className="bug-thebutton"
                         to="https://kdocs.cn/l/chdVh5CuGxxr">
                         <Translate>Learn More</Translate>
-                      </Link>
+                      </Link> */}
                     </div>
-                    <div className="bug-svg-p"><ArrowLeft></ArrowLeft></div>
-                    <span className="buttom-span-p"><Translate>Click "Learn More" to access detailed operational instructions.</Translate></span>
+                    {/* <div className="bug-svg-p"><ArrowLeft></ArrowLeft></div> */}
+                    {/* <span className="buttom-span-p"><Translate>Click "Learn More" to access detailed operational instructions.</Translate></span> */}
                 </div>
             </div>
         </div>
