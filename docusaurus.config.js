525f2bc370aaf0df7707565b0225293a91766b35
liangxiangyu <liangxy@highgo.com>
2024-07-05 15:57:25 +0800

    release v3.3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,7 +71,7 @@ const config = {
           },
           {
             label: 'Installation',
-            href: 'https://docs.ivorysql.org/en/ivorysql-doc/v3.2/v3.2/3#quick-installation',
+            href: 'https://docs.ivorysql.org/en/ivorysql-doc/v3.3/v3.3/3#quick-installation',
             position: 'left'
           },
           {
