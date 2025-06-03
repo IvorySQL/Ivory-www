83ee2fd4870c9899f1783f55640685d40dc1605d
JiaoShuntian <jiaoshuntian@highgo.com>
2025-06-03 14:06:02 +0800

    adjust margin

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,7 +59,7 @@ function HomepageHeader() {
                   <div className={styles.heroCenterImage}>
                     <h1 className="hero__title"><Translate>IvorySQL</Translate></h1>
                     <p className="hero__subtitle"><Translate>Open Source Oracle Compatible PostgreSQL</Translate></p>
-                    <div className={styles.buttons} margin-horiz--md>
+                    <div className={styles.buttons}>
                       <Link
                         className="button button--primary button--lg"
                         target="_blank"
